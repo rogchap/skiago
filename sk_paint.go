98d191295b01706a19150200d9c1bb6016bc778c
Roger Chapman <rogchap@gmail.com>
2024-03-20 14:05:17 +1100

    paint takes a color struct

--- sk_paint.go
+++ sk_paint.go
@@ -3,16 +3,18 @@ package skia
 //#include "sk_paint.h"
 import "C"
 
+import "image/color"
+
 type Paint struct {
 	handle *C.sk_paint_t
 }
 
-func NewPaint(a, r, g, b uint8) *Paint {
+func NewPaint(c color.RGBA) *Paint {
 	p := C.sk_paint_new()
-	C.sk_paint_set_color(p, colorSetARGB(a, r, g, b))
+	C.sk_paint_set_color(p, colorSetRGBA(c))
 	return &Paint{p}
 }
 
-func colorSetARGB(a, r, g, b uint8) C.sk_color_t {
-	return C.sk_color_t((uint32(a) << 24) | (uint32(r) << 16) | (uint32(g) << 8) | uint32(b))
+func colorSetRGBA(c color.RGBA) C.sk_color_t {
+	return C.sk_color_t((uint32(c.A) << 24) | (uint32(c.R) << 16) | (uint32(c.G) << 8) | (uint32(c.B)))
 }
