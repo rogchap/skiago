8d67f15629a9284824a3baa659ac66779600e348
Roger Chapman <rogchap@gmail.com>
2024-03-20 11:30:33 +1100

    include libskia for darwin_arm64

--- cgo.go
+++ cgo.go
@@ -2,4 +2,6 @@ package skia
 
 // #cgo LDFLAGS: -lskia
 // #cgo CXXFLAGS: -I${SRCDIR}/lib -std=c++17 -fvisibility-inlines-hidden -fno-exceptions -fno-rtti
+// #cgo darwin LDFLAGS: -framework CoreFoundation -framework CoreGraphics -framework CoreText -framework CoreServices
+// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/darwin_arm64
 import "C"
